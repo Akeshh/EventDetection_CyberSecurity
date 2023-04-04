51e139da48f926beb320971319e2fd1d95a765f5
Akeshh <95958741+Akeshh@users.noreply.github.com>
2023-04-04 23:46:34 +0800

    Update run_MAVEN.sh

--- BERT+CRF/run_MAVEN.sh
+++ BERT+CRF/run_MAVEN.sh
@@ -1,15 +1,15 @@
 python3 run_maven.py \
-    --data_dir ../maven/ge11 \ #path to the raw MAVEN data files
+    --data_dir ../Data/ \ #path to the raw MAVEN data files
     --model_type bertcrf \
-    --model_name_or_path microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext \
-    --output_dir ./MAVEN \ #path to dump checkpoints
+    --model_name_or_path bert-base-uncased \
+    --output_dir ./output \ #path to dump checkpoints
     --max_seq_length 256 \
     --do_lower_case \
-    --per_gpu_train_batch_size 16 \
-    --per_gpu_eval_batch_size 16 \
-    --gradient_accumulation_steps 8 \
+    --per_gpu_train_batch_size 8 \
+    --per_gpu_eval_batch_size 8 \
+    --gradient_accumulation_steps 16 \
     --learning_rate 5e-5 \
-    --num_train_epochs 20 \
+    --num_train_epochs 50 \
     --save_steps 100 \
     --logging_steps 100 \
     --seed 0 \
